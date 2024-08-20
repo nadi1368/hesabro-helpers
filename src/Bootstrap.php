43808b6546af133bba94b9deebf6091305571884
Moslem Tayari <avapardazmota@gmail.com>
2024-08-20 14:42:58 +0330

    fix: move registers to bootstarp

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,13 +2,16 @@
 
 namespace hesabro\helpers;
 
+use hesabro\helpers\components\Env;
+use hesabro\helpers\components\Helper;
+use hesabro\helpers\components\Jdf;
+use hesabro\helpers\components\PhpNewVer;
+use kartik\grid\Module as GridModule;
 use yii\base\Application;
 use yii\base\BootstrapInterface;
 
 class Bootstrap implements BootstrapInterface
 {
-    private array $requiredConfigs = [];
-
     /**
      * Bootstrap method to be called during application bootstrap stage.
      * @param Application $app the application currently running
@@ -16,5 +19,26 @@ class Bootstrap implements BootstrapInterface
     public function bootstrap($app)
     {
         $app->params['bsVersion'] = 4;
+
+        $app->setComponents([
+            'helper' => [
+                'class' => Helper::class
+            ],
+            'phpNewVer' => [
+                'class' => PhpNewVer::class
+            ],
+            'jdf' => [
+                'class' => Jdf::class
+            ],
+            'env' => [
+                'class' => Env::class
+            ]
+        ]);
+
+        $app->setModules([
+            'gridview' => [
+                'class' => GridModule::class
+            ]
+        ]);
     }
 }
--- src/Module.php
+++ src/Module.php
@@ -2,38 +2,12 @@
 
 namespace hesabro\helpers;
 
-use hesabro\helpers\components\Env;
-use hesabro\helpers\components\Helper;
-use hesabro\helpers\components\Jdf;
-use hesabro\helpers\components\PhpNewVer;
 use yii\base\Module as BaseModule;
-use kartik\grid\Module as GridModule;
 
 class Module extends BaseModule
 {
     public function init(): void
     {
         parent::init();
-
-        $this->setComponents([
-            'helper' => [
-                'class' => Helper::class
-            ],
-            'phpNewVer' => [
-                'class' => PhpNewVer::class
-            ],
-            'jdf' => [
-                'class' => Jdf::class
-            ],
-            'env' => [
-                'class' => Env::class
-            ]
-        ]);
-
-        $this->setModules([
-            'gridview' => [
-                'class' => GridModule::class
-            ]
-        ]);
     }
 }
