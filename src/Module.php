57d8c7c44d785c6e7746355fa8e5cfe08f6f8423
Moslem Tayari <avapardazmota@gmail.com>
2024-09-18 09:47:48 +0330

    remove component and module initial

--- src/Module.php
+++ src/Module.php
@@ -2,11 +2,6 @@
 
 namespace hesabro\helpers;
 
-use hesabro\helpers\components\Env;
-use hesabro\helpers\components\Helper;
-use hesabro\helpers\components\Jdf;
-use hesabro\helpers\components\PhpNewVer;
-use kartik\grid\Module as GridModule;
 use yii\base\Module as BaseModule;
 
 class Module extends BaseModule
@@ -14,26 +9,5 @@ class Module extends BaseModule
     public function init(): void
     {
         parent::init();
-
-        $this->setComponents([
-            'helper' => [
-                'class' => Helper::class
-            ],
-            'phpNewVer' => [
-                'class' => PhpNewVer::class
-            ],
-            'jdf' => [
-                'class' => Jdf::class
-            ],
-            'env' => [
-                'class' => Env::class
-            ]
-        ]);
-
-        $this->setModules([
-            'gridview' => [
-                'class' => GridModule::class
-            ]
-        ]);
     }
 }
